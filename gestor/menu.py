5321d82b5614a40d8f194c41b5d848939f18dbe6
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-09-20 15:06:17 +0000

    cambios

--- gestor/menu.py
+++ gestor/menu.py
@@ -1,4 +1,8 @@
 import os
+import helpers
+import database as db
+
+helpers.limpiar_pantalla()
 
 def iniciar():
    while True:
@@ -15,17 +19,47 @@ def iniciar():
      print("========================")
      opcion = input("> ")
      os.system('clear') # cls en Windows
-     if opcion == '1':
-       print("Listando los clientes...\n")
-     if opcion == '2':
-       print("Buscando un cliente...\n")
-     if opcion == '3':
-       print("Añadiendo un cliente...\n")
-     if opcion == '4':
-       print("Modificando un cliente...\n")
-     if opcion == '5':
-       print("Borrando un cliente...\n")
-     if opcion == '6':
-       print("Saliendo...\n")
-       break
+  if opcion == '1':
+   print("Listando los clientes...\n")
+   for cliente in db.Clientes.lista:
+     print(cliente)
+  if opcion == '2':
+   print("Buscando un cliente...\n")
+   dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+   cliente = db.Clientes.buscar(dni)
+   print(cliente) if cliente else print("Cliente no encontrado.")
+  if opcion == '3':
+    if opcion == '3':
+ print("Añadiendo un cliente...\n")
+ # Comprobación de DNI válido
+ while 1:
+ dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1
+char)").upper()
+ if helpers.dni_valido(dni, db.Clientes.lista):
+ break
+ nombre = helpers.leer_texto(
+ 2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+ apellido = helpers.leer_texto(
+ 2, 30, "Apellido (de 2 a 30 chars)").capitalize()
+ db.Clientes.crear(dni, nombre, apellido)
+if opcion == '4':
+ print("Modificando un cliente...\n")
+ dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+ cliente = db.Clientes.buscar(dni)
+ if cliente:
+   nombre = helpers.leer_texto(
+     2, 30, f"Nombre (de 2 a 30 chars)[{cliente.nombre}]").capitalize()
+   apellido = helpers.leer_texto( 2, 30, f"Apellido (de 2 a 30 chars)[{cliente.apellido}]").capitalize()
+   db.Clientes.modificar(cliente.dni, nombre, apellido)
+   print("Cliente modificado correctamente.")
+ else:
+   print("Cliente no encontrado.")
+if opcion == '5':
+ print("Borrando un cliente...\n")
+ dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+ print("Cliente borrado correctamente.") if db.Clientes.borrar(
+ dni) else print("Cliente no encontrado.")
+if opcion == '6':
+ print("Saliendo...\n")
+break
 input("\nPresiona ENTER para continuar...")
--- gestor/test/test_database.py
+++ gestor/test/test_database.py
@@ -1,5 +1,6 @@
 import copy
 import unittest
+import helpers
 import database as db
 
 class TestDatabase(unittest.TestCase):
@@ -34,6 +35,12 @@ class TestDatabase(unittest.TestCase):
    cliente_borrado = db.Clientes.borrar('48H')
    cliente_rebuscado = db.Clientes.buscar('48H')
    self.assertNotEqual(cliente_borrado, cliente_rebuscado)
+
+ def test_dni_valido(self):
+   self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))
+   self.assertFalse(helpers.dni_valido('23223S', db.Clientes.lista))
+   self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
+   self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))  
    
 if __name__ == '__main__':
  unittest.main()
