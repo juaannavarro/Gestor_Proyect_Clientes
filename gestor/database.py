37d71fb558c18f2276354b73e241368d8193eb04
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-10-04 13:20:34 +0200

    Update database.py

--- gestor/database.py
+++ gestor/database.py
@@ -1,3 +1,10 @@
+import csv
+import config
+import pandas as pd  
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from scipy import stats
 class Cliente:
  def __init__(self, dni, nombre, apellido):
    self.dni = dni
@@ -8,30 +15,46 @@ class Cliente:
 
 class Clientes:
  # Lista de clientes
-   lista = []
-   @staticmethod
-   def buscar(dni):
-     for cliente in Clientes.lista:
-       if cliente.dni == dni:
+  lista = []
+  with open(config.DATABASE_PATH, newline="\n") as archivo:
+    reader = csv.reader(archivo)
+    for dni, nombre, apellido in reader:
+      cliente = Cliente(dni, nombre, apellido)
+      lista.append(cliente)
+  print(lista)
+  @staticmethod
+  def buscar(dni):
+    for cliente in Clientes.lista:
+      if cliente.dni == dni:
         return cliente
 
   @staticmethod
   def crear(dni, nombre, apellido):
-     cliente = Cliente(dni, nombre, apellido)
-     Clientes.lista.append(cliente)
-     return cliente
+    cliente = Cliente(dni, nombre, apellido)
+    Clientes.lista.append(cliente)
+    Clientes.guardar() 
+    return cliente
     
- @staticmethod
- def modificar(dni, nombre, apellido):
-   for i, cliente in enumerate(Clientes.lista):
-     if cliente.dni == dni:
-       Clientes.lista[i].nombre = nombre
-       Clientes.lista[i].apellido = apellido
-       return Clientes.lista[i]
+  @staticmethod
+  def modificar(dni, nombre, apellido):
+    for i, cliente in enumerate(Clientes.lista):
+      if cliente.dni == dni:
+        Clientes.lista[i].nombre = nombre
+        Clientes.lista[i].apellido = apellido
+        Clientes.guardar()
+        return Clientes.lista[i]
        
- @staticmethod
- def borrar(dni):
-   for i, cliente in enumerate(Clientes.lista):
-     if cliente.dni == dni:
-       cliente = Clientes.lista.pop(i)
-     return cliente
+  @staticmethod
+  def borrar(dni):
+    for i, cliente in enumerate(Clientes.lista):
+      if cliente.dni == dni:
+        cliente = Clientes.lista.pop(i)
+        Clientes.guardar()
+        return cliente
+
+  @staticmethod
+  def guardar():
+    with open(config.DATABASE_PATH, newline="\n") as fichero:
+      writer = csv.writer(fichero, delimiter=";")
+      for cliente in Clientes.lista:
+        writer.writerow([cliente.dni, cliente.nombre, cliente.apellido])
