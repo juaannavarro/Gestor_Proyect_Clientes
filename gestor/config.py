9d2959d34ca4f77eb1782eb620ec9f125e31086b
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-10-06 18:18:32 +0200

    Update config.py

--- gestor/config.py
+++ gestor/config.py
@@ -1,6 +1,7 @@
 import sys
+import csv
 
-DATABASE_PATH = 'clientes.csv'
+DATABASE_PATH = '/Users/juanlu_navarro/Documents/Carrera Juan/programacion/Gestor_Proyect_Clientes/gestor/clientes.csv'
 
 if 'pytest' in sys.argv[0]:
     DATABASE_PATH = 'tests/clientes_test.csv'
